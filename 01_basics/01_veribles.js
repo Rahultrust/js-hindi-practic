fdaa94be19f766b7df0f6f8f803448998d0ee7e5
RAHUL KUMAR <119796740+Rahultrust@users.noreply.github.com>
2024-01-07 19:06:12 +0000

    datatypes

---
+++ 01_basics/01_veribles.js
@@ -0,0 +1,19 @@
+const accountId = 1234453
+let accountEmail = "rahul.kumar525757@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2
+
+accountEmail = "rk525757@gmail.com"
+accountPassword = "212121"
+accountCity = "noida"
+let accountState;// because let is decaliear
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+perfer not to use var 
+because of issue in bolock scope and function scope
+
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";//treat all js code as newer version
+
+//alert( 3 + 3) // we are using nodejs, not browser
+
+console.log( 3 
+    + 3) // code readbility should be high
+
+console.log("Rahul")
+
+//   javaScript ki orignel docment= tc39.es
+// mdn 
+
+
+let name = "rahul"
+let age = 18
+let isLoggedIn = false  
+
+//number => 2 to power 53
+// bigint
+// string =>""
+//boolean => true/false
+//null => standalone value ./ null is identifi of empty value
+// undefined => avhi value assign nahi hui hai
+//symbol => unique ke liye use kerte hai
+
+//object
+
+console.log(typeof"rahul"); // rahul is string
+console.log(typeof age); // age is number
+console.log(typeof null);// null is a object  
+console.log(typeof undefined);// undefind
